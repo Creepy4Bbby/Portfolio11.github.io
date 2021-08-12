b1210a5a5a8186d747695830ab88015e0053f1f8
gcsa94290 <80926176+gcsa94290@users.noreply.github.com>
2021-08-12 14:09:15 +0200

    ad

--- Code/Contenue/Experiences5.php
+++ Code/Contenue/Experiences5.php
@@ -278,11 +278,12 @@ include 'Header.php';
 
 			<!-- Accès direct à : --> <!-- image bouton -->
 
+			<html> <body> <span style="position: absolute; top: 150px; left: 1000px;">
 <ul>
 <div align="center"><li><a href="#Entreprise">Espace entreprise</a> ;</li><img src="images/2p5.png" WIDTH=150 HEIGHT=150></a></div>
 <div align="center"><li><a href="#Scolaire">Espace scolaire</a> ;</li><img src="images/1p5.png" WIDTH=150 HEIGHT=150></a></div>
 </ul>
-
+</span> </body> </html>
 <!-- Partie entreprise -->
 <h2 id="Entreprise">Espace entreprise</h2>
 Toutes vos experiences en entreprise doivent etre mis ici :
