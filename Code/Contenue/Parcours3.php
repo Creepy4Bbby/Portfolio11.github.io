e0327166a7f6ec89cefe33686f364cdc2ca69a17
Creepy4Bbby <82474796+Creepy4Bbby@users.noreply.github.com>
2021-11-24 16:23:25 +0100

    parcours timelines vertical et horizontale

--- Code/Contenue/Parcours3.php
+++ Code/Contenue/Parcours3.php
@@ -30,11 +30,32 @@
 									 <div align="center"><h1><a href="PageAccueil.php">Parcours</h1></a></di>	
 									 
 								</header>
+
+
+<!-- Section -->
+
+									<div class="features">
+										<article>
+										<span class="icon solid fas fas fa-briefcase"></span>
+											<div class="content">
+											<div align="center"><a href="#Scolaire"><h3 id="#">SCOLAIRE</h3></a></div>
+												<p></p>
+											</div>
+										</article>
+										<article>
+										<span class="icon solid fas fas fa-graduation-cap"></span>
+											<div class="content">
+											<div align="center"><a href="#Entreprise"><h3 id="#">PROFESSIONNEL</h3></a></div>
+												<p></p>
+											</div>
+										</article>
+									
+									</div>
+								
 <!-- Diplomes -->
 								
 									<header class="major">
-										<br>
-										<h2>Diplômes</h2>
+										<h2 id="Scolaire">Diplômes</h2>
 									</header>
 									
 							
@@ -67,23 +88,73 @@
 <!-- //Experience pro  -->
 									<br>
 									<header class="major">
-										<h2>Expériences professionnelle</h2>
+										<br>
+										<h2 id="Entreprise">Expériences professionnelle</h2>
 									</header>
+								
 									<div class="features">
 										
 									</div>
-								</section>
+								
+
+<!-- ________________________________ -->
+
+	
+<div id="workexperience" class="sectionClass">
+  <div class="row ">
+    <div class="fullWidth eight columns">
+      <ul class="cbp_tmtimeline">
+        <li>
+          <div class="cbp_tmicon cbp_tmicon-phone">
+            <i class="faPra fa-briefcase"></i>
+          </div>
+          <div class="cbp_tmlabel wow fadeInRight animated">
+            <h3>Web developer</h3>
+            <div class="date">
+              <i class="fa fa-calendar"></i>April 2014 - Current
+            </div>
+            <h4><i class="fa fa-flag"></i>Davic Company, Bratislava</h4>
+            <p class="projectParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit obcaecati ipsa quae, iusto laudantium qui, nisi eum modi perspiciatis quasi facilis corporis iure soluta enim incidunt itaque aspernatur sequi tempora.</p>
+          </div>
+        </li>
+        <li>
+          <div class="cbp_tmicon cbp_tmicon-screen">
+            <i class="faPra fa-briefcase"></i>
+          </div>
+          <div class="cbp_tmlabel wow fadeInRight animated">
+            <h3>Web designer</h3>
+            <h4><i class="fa fa-flag"></i>Fannous Company, Prague</h4>
+            <div class="date"><i class="fa fa-calendar"></i>June 2012 - April 2014</div>
+            <p class="projectParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quasi perspiciatis, aliquid sed maiores accusamus. Adipisci quidem nostrum quos quae doloremque esse a, ipsum earum, recusandae omnis dignissimos et sint.</p>
+          </div>
+        </li>
+        <li>
+          <div class="cbp_tmicon cbp_tmicon-mail">
+            <i class="faPra fa-briefcase"></i>
+          </div>
+          <div class="cbp_tmlabel wow fadeInRight animated">
+            <h3>Web </h3>
+            <h4><i class="fa fa-flag"></i>Techixs Company, London</h4>
+            <div class="date"><i class="fa fa-calendar"></i>November 2009 - June 2012</div>
+            <p class="projectParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla labore atque alias ipsa, nam quod rerum repellat cumque, aliquam sequi vitae voluptatibus cum soluta incidunt tempore accusamus eius sed excepturi!Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Tempora natus veritatis aperiam repellendus dolor vel, expedita assumenda eos, mollitia quae ullam esse voluptas vero. Dolores culpa eaque vitae eum quibusdam?</p>
+
+          </div>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</div>
+
+</section>
 
 
-
+<!-- ______________________________________ -->
 
 						</div>
 					</div>
-<!-- _______________________ -->
 
-
-<!-- ______________________________________________ -->
-						<!-- MENU -->
 
 
 				<!-- Sidebar -->
