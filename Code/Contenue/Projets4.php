f016ce52a8c29b93a4042a0f933df3a3f36c4812
Creepy4Bbby <82474796+Creepy4Bbby@users.noreply.github.com>
2022-02-04 12:08:55 +0100

    t

--- Code/Contenue/Projets4.php
+++ Code/Contenue/Projets4.php
@@ -175,19 +175,19 @@
 										<article>
 											<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
 											<h3>Site web assistante maternelle</h3>
-											<p>Ma mère ayant besoin d'un plateforme pour communiquer avec les parents des enfants qu'elle garde j'ai donc creer un site à cet effet</p>
+											<p>Ma mère ayant besoin d'une plateforme pour communiquer avec les parents des enfants qu'elle garde j'ai donc creer un site à cet effet</p>
 											<ul class="actions">
 												<li><a href="#" class="button">More</a></li>
 											</ul>
 										</article>
-										<article>
+										<!-- <article>
 											<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
 											<h3>Amet varius aliquam</h3>
 											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
 											<ul class="actions">
 												<li><a href="#" class="button">More</a></li>
 											</ul>
-										</article>
+										</article> -->
 									</div>
 								</section>
 								
