daacb41eb271f453d89e001577393fc84293bff8
Dolores HUET <82474796+Creepy4Bbby@users.noreply.github.com>
2022-02-14 13:42:33 +0100

    PDF reussi

--- Code/Contenue/M2L.php
+++ Code/Contenue/M2L.php
@@ -1,59 +1,4 @@
-<!DOCTYPE HTML>
 
-<html>
-	<head>
-		<title>Portfolio</title>
-		<meta charset="utf-8" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" /> 
-		<link rel="stylesheet" href="assets/css/main.css" /> 
-        <link rel="stylesheet" href="Header.html" />
-	</head>
-	<body class="is-preload">
-
-	
-
-		<!-- Wrapper -->
-			<div id="wrapper">
-
-				<!-- Main -->
-					<div id="main">
-						<div class="inner">
-							<!-- Header -->
-								<header id="header">
-									<a href="Contacts8.php" class="logo"><strong>Me suivre...&nbsp </strong> &nbsp &nbsp &nbsp ici &nbsp →  <div align="center"><h1><a href="PageAccueil.php">Projets</h1></a></div>  </a>
-									<ul class="icons">
-									
-										<li><a href="https://github.com/Creepy4Bbby" class="icon brands fab fa-github"><span class="label">GitHub</span></a></li>
-										<li><a href="Contacts8.php" class="icon brands fab fa-linkedin "><span class="label">Linkdin</span></a></li>
-										
-									</ul>
-								</header>
-
- 
-								<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
- 
-
-								<!-- HAUT DE PAGE  -->
- <script>
-			 jQuery(function(){
-				 $(function () {
-					 $(window).scroll(function () {
-						 if ($(this).scrollTop() > 200 ) { 
-							 $('#scrollUp').css('right','30px');
-						 } else { 
-							 $('#scrollUp').removeAttr( 'style' );
-						 }
-  
-					 });
-				 });
-			 });
- </script>
-  
-
-						
-												<a href="Projets4.php"  id="Scolaire" class="button"> Retour à la page Projets</a>
-											
-										
 									
 <?php 
   // Le chemin du fichier (path) 
@@ -78,13 +23,6 @@
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
 
-<!-- icône haut de page -->
-			<div id="scrollUp">
-<a href="#top" class="icon solid fas fa-cloud-upload-alt fa-2x"></a>
-</div>
-
-	</body>
-</html>
 
 <script>
 
